e6c726cba8afb7fdbd676b544d6116e45f6c5e3a
haitham ali <haithamaly20@gmail.com>
2026-08-03 14:29:20 +0300

    Update SCRIPT_URL to new deployment and bump cache version

--- js/config.js
+++ js/config.js
@@ -18,17 +18,16 @@ const CONFIG = {
 
   APP_NAME: "خطة موجهي الضبعة",
 
-  APP_VERSION: "1.0.2",
+  APP_VERSION: "1.0.3",
 
-  CACHE_VERSION: "1.0.2",
-
+  CACHE_VERSION: "1.0.3",
 
   /* ========================================================
      Google Sheets
   ======================================================== */
 
   // رابط Google Apps Script Web App
-  SCRIPT_URL: "https://script.google.com/macros/s/AKfycbw1mMGuWQ8KCSh20ekTmFtGYRtfuM7LP7k2Ic4wvPHsbNXfLNrYpcGntqtQcGmKdiwa/exec",
+  SCRIPT_URL:  "https://script.google.com/macros/s/AKfycby0GR1JlVs3OKje_vADEG3BQP3oKBGYIhWadJUWtsVjO622Hh2e3wff7ut2-SH2bdp9/exec",
 
   // اسم ورقة العمل
   SHEET_NAME: "",
